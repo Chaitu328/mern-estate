fbe8a4d89843e7772c02833a526fb83df42eb911
chaitu328 <konakallachaitanya@gmail.com>
2025-04-07 18:42:16 +0530

    create a middleware and function to handle possible errors

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,7 +1,8 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import { errorHandler } from "../Utils/error.js";
 
-export const signupController = async (req, res) => {
+export const signupController = async (req, res,next) => {
     const { username, email, password } = req.body;
     const hashedPassword = bcrypt.hashSync(password, 8);
     const newUser = new User({
@@ -13,7 +14,6 @@ export const signupController = async (req, res) => {
         await newUser.save();
         res.status(201).json({ message: "User signed up successfully"});
     } catch (error) {
-        res.status(500).json(error.message)
+        next(error);
     }
-    
 }
--- api/server.js
+++ api/server.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import useRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
+import { errhander } from './middleware/error.middleware.js'
 dotenv.config()
 
 mongoose.connect(process.env.MONGO_URI, {
@@ -19,7 +20,7 @@ app.use('/api/user',useRouter)
 app.use(express.json())
 app.use('/api/auth',authRouter)
 
-
+app.use(errhander)
 app.listen(3000,()=>{
     console.log("Server is running on Port 3000")
 })
