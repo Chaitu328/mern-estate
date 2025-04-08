ddd7d1fb77fc703e0a4faeaa28e95536e82ac3d5
chaitu328 <konakallachaitanya@gmail.com>
2025-04-08 16:05:19 +0530

    complete sign up functionality

--- api/middleware/error.middleware.js
+++ api/middleware/error.middleware.js
@@ -1,8 +1,8 @@
-export const errhander = (err, req, res, next) => {
+export const errMiddleware = (err, req, res, next) => {
     const statusCode = err.statusCode || 500;
     const message = err.message || 'Internal Server Error';
     res.status(statusCode).json({
-        status: 'error',
+        success: false,
         statusCode,
         message,
     });
