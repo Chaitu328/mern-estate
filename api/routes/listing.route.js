edebb54f82dd0f23d332d32d393bef117cd4cf10
chaitu328 <konakallachaitanya@gmail.com>
2025-04-21 15:48:39 +0530

    complete delete api route and delete user listing functionality

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,10 +1,10 @@
 import express from 'express'
-import { createListing } from '../controller/listing.controller.js'
+import { createListing,deleteListing } from '../controller/listing.controller.js'
 import { verifyUserToken } from '../Utils/verifyUser.js'
 
 const router = express.Router()
 
 router.post('/create',verifyUserToken,createListing)
-
+router.delete('/delete/:id',verifyUserToken,deleteListing)
 
 export default router
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -124,6 +124,24 @@ export default function Profile() {
       setShowListingError(true,error.message)
     }
   }
+  const handleListingDelete=async(listingId)=>{
+    try {
+      const res = await fetch(`/api/listing/delete/${listingId}`,{
+        method: 'DELETE',
+      })
+      const data = await res.json()
+      if(data.success === false){
+        console.log(data.message)
+        return
+      }
+      setUserListings((prev) =>
+        prev.filter((listing) => listing._id !== listingId)
+      );
+      
+    } catch (error) {
+      console.log(error)
+    }
+  }
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className='text-3xl font-semibold text-center my-7'>Profile
@@ -214,7 +232,7 @@ export default function Profile() {
             </Link>
 
             <div className="flex flex-col gap-2">
-              <button className="text-red-700 uppercase hover:cursor-pointer" type="button">delete</button>
+              <button onClick={()=>handleListingDelete(listing._id)} className="text-red-700 uppercase hover:cursor-pointer" type="button">delete</button>
               <button className="text-green-700 uppercase hover:cursor-pointer" type="button">edit</button>
             </div>
           </div>
