2b5872951bc16d0e3e66e09f55e03eba9760d5ed
chaitu328 <konakallachaitanya@gmail.com>
2025-04-22 16:34:38 +0530

    create update listing api route

--- api/controller/listing.controller.js
+++ api/controller/listing.controller.js
@@ -13,12 +13,28 @@ export const createListing = async (req, res,next) => {
 export const deleteListing =async (req,res,next)=>{
     const listing = await Listing.findById(req.params.id)
     if(!listing){
-        return errorHandler(404,'Listing not found!')
+        return next(errorHandler(404,'Listing not found!'))
     }
-    if(req.user.id !== listing.userRef) return errorHandler(401,'You can only delete your own listings!')
+    if(req.user.id !== listing.userRef) return next(errorHandler(401,'You can only delete your own listings!'))
     try {
         await Listing.findByIdAndDelete(req.params.id)
         res.status(200).json('Listing has been deleted!');
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const updateListing=async(req,res,next)=>{
+    const listing = await Listing.findById(req.params.id)
+    if(!listing) return next(errorHandler(404, 'Listing not found!')); 
+    if(req.user.id !== listing.userRef) return next(errorHandler(404, 'Listing not found!')); 
+    try {
+        const updatedListing = await Listing.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            {new:true}
+        )
+        await res.status(200).json(updatedListing)
     } catch (error) {
         next(error)
     }
--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,10 +1,11 @@
 import express from 'express'
-import { createListing,deleteListing } from '../controller/listing.controller.js'
+import { createListing,deleteListing,updateListing } from '../controller/listing.controller.js'
 import { verifyUserToken } from '../Utils/verifyUser.js'
 
 const router = express.Router()
 
 router.post('/create',verifyUserToken,createListing)
 router.delete('/delete/:id',verifyUserToken,deleteListing)
+router.post('/update/:id',verifyUserToken,updateListing)
 
 export default router
